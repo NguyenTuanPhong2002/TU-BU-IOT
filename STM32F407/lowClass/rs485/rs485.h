a582acfd778cdc5dfb246510332b4417543ef488
Trump2003 <skttrung2003@gmail.com>
2023-10-04 13:36:40 +0700

    rs485-trump2003

--- STM32F407/lowClass/rs485/rs485.h
+++ STM32F407/lowClass/rs485/rs485.h
@@ -8,6 +8,20 @@
 #ifndef RS485_RS485_H_
 #define RS485_RS485_H_
 
+typedef struct
+{
+	GPIO_TypeDef *RS485_Power_Port;
+	uint16_t RS485_Power_Pin;
+	GPIO_TypeDef *RS485_DE_Port;
+	uint16_t RS485_DE_Pin;
+	GPIO_TypeDef *RS485_RE_Port;
+	uint16_t RS485_RE_Pin;
+	UART_HandleTypeDef *huart;
+	char TxBuffer[256];
+	char RxBuffer[256];
 
+} RS485;
+void RS485_Transmit(RS485 *me);
+void RS485_Receive(RS485 *me);
 
 #endif /* RS485_RS485_H_ */
